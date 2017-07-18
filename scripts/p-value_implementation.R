af345cd8bc14fd97bdfc305dd308b85917fce264
edoardo pedrini <pedrini.edoardo@gmail.com>
2017-07-18 09:39:32 -0700

    Update p-value_implementation.R

--- scripts/p-value_implementation.R
+++ scripts/p-value_implementation.R
@@ -1,17 +1,11 @@
+# input the data
 df <- read.csv("testHK.csv")
 df$Tumor.type<-as.character(df$Tumor.type)
 Normfinder("testHK.csv",ctVal = FALSE)
 norm <- Normfinder("testHK.csv",ctVal = FALSE)
 ord <- rownames(norm$Ordered)
 
-# Normfinder("testHK.csv",ctVal = TRUE)
-
-# # try a bigger dataset
-# df
-# 
-# dff_big<-
-
-# see the boxplots
+# data manipulation in order to rearrange the dataset
 df2<-t(df)
 # make the first row as column
 colnames(df2)<-df2[1,]
@@ -21,17 +15,18 @@ df3<-as.data.frame(df2[-1,])
 df3_2 <- as.data.frame(lapply(df3,function(x){
   return(as.numeric(as.character(x)))
 }))
-# arrange the df for ggplot2
+# rearrange the df for ggplot2
 library(tidyr)
-library(ggplot2)
+# library(ggplot2)
 df3_3 <- gather(data = df3_2,key = gene,value = exp,-group)
 
-# my approach would be by using the p-value as ordering criterion 
-
+# my approach would be by using the p-value as ordering criterion
+# so far my approach can only deal with two "treatment".
+# run a t test to compare the two groups
 list_df3_3<-split(df3_3,f = df3_3$gene)
 list2<-lapply(list_df3_3,function(x){
   t.test(x$exp[x$group==1],x$exp[x$group==2])$p.value
 })
-
+# sort the result by the highest p_value
 value<-unlist(list2)
 sort(value,decreasing = T)
