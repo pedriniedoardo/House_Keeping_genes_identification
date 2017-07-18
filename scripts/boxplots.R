a335694ee7940b2dba96d9107601e6004b2b6e4f
edoardo pedrini <pedrini.edoardo@gmail.com>
2017-07-18 10:42:02 -0700

    Update boxplots.R

--- scripts/boxplots.R
+++ scripts/boxplots.R
@@ -1,3 +1,4 @@
+# shape the data in order to be accepted by ggplot
 df <- read.csv("testHK.csv")
 df$Tumor.type<-as.character(df$Tumor.type)
 Normfinder("testHK.csv",ctVal = FALSE)
@@ -26,7 +27,7 @@ library(tidyr)
 library(ggplot2)
 df3_3 <- gather(data = df3_2,key = gene,value = exp,-group)
 
-# order the genes by stability
+# order the genes by stability according to normfinder
 normfinder <- ggplot(df3_3,aes(x=factor(gene,levels = ord),y=exp,fill=factor(group)))+
   geom_boxplot()+
   theme(axis.text.x = element_text(angle = 60, hjust = 1))+
@@ -38,11 +39,12 @@ p_value <- ggplot(df3_3,aes(x=factor(gene,levels = names(sort(value,decreasing =
   theme(axis.text.x = element_text(angle = 60, hjust = 1))+
   ggtitle("p_value ranked genes")+xlab("")
 
-#genorm
+# rank according to genorm
 genorm <- ggplot(df3_3,aes(x=factor(gene,levels = rank2$ranking),y=exp,fill=factor(group)))+
   geom_boxplot()+
   theme(axis.text.x = element_text(angle = 60, hjust = 1))+
   ggtitle("genorm ranked genes")+xlab("")
-#build the first grob
+
+# build the first grob
 library(gridExtra)
 grob1 <- do.call("grid.arrange", c(list(normfinder,genorm,p_value), ncol=1))
